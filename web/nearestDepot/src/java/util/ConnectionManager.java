3bdec9d6b1139c82610a3479e4922e9ca77b3da1
yukun.yin.2016@sis.smu.edu.sg <KY1N2XRr2.1>
2018-03-17 16:19:36 +0800

    added more websssssssss

---
+++ web/nearestDepot/src/java/util/ConnectionManager.java
@@ -0,0 +1,118 @@
+package util;
+
+import java.sql.*;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A class that manages connections to the database. It also has a utility
+ * method that close connections, statements and resultsets
+ */
+public class ConnectionManager {
+
+
+
+    /**
+     * Static constructor
+     */
+    static {
+        readDatabaseProperties();
+        initDBDriver();
+    }
+
+    /**
+     * Loads connection properties from connection.properties file
+     */
+            static String host = "localhost";
+            static String port = "3306";
+            static String dbName = "busdepot";
+            static String dbUser = "root";
+            static String dbPassword = "";
+            static String dbURL = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+    private static void readDatabaseProperties() {
+        InputStream is = null;
+        try {
+            // Retrieve properties from connection.properties via the CLASSPATH
+            // WEB-INF/classes is on the CLASSPATH
+
+            // load database connection details
+
+        } catch (Exception ex) {
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING, "Unable to close connection.properties", ex);
+                }
+            }
+        }
+    }
+
+    /**
+     * initializes the JBDC driver
+     */
+    private static void initDBDriver() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // unable to load properties file
+            String message = "Unable to find JDBC driver for MySQL.";
+
+            System.out.println(message);
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, message, ex);
+            throw new RuntimeException(message, ex);
+        }
+    }
+
+    /**
+     * Gets a connection to the database
+     *
+     * @return the connection
+     * @throws SQLException if an error occurs when connecting
+     */
+    public static Connection getConnection() throws SQLException {
+        String message = "dbURL: " + dbURL
+                + "  , dbUser: " + dbUser
+                + "  , dbPassword: " + dbPassword;
+        Logger.getLogger(ConnectionManager.class.getName()).log(Level.INFO, message);
+
+        return DriverManager.getConnection(dbURL, dbUser, dbPassword);
+    }
+
+    /**
+     * close the given connection, statement and resultset
+     *
+     * @param conn the connection object to be closed
+     * @param stmt the statement object to be closed
+     * @param rs the resultset object to be closed
+     */
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
+                    "Unable to close ResultSet", ex);
+        }
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
+                    "Unable to close Statement", ex);
+        }
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.WARNING,
+                    "Unable to close Connection", ex);
+        }
+    }
+}
