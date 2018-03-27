2604b973c3950f97e27c8ab4d9f836ebaa179a5f
Ho Wei Hong <weihong.ho.2016@sis.smu.edu.sg>
2018-03-27 21:05:21 +0800

    Update TrainListener
    Fixed flushing issue
    Fixed hardcoded URL

--- TrainBreakDown/TrainListener.java
+++ TrainBreakDown/TrainListener.java
@@ -4,78 +4,72 @@ import java.io.*;
 import java.lang.*;
 import com.tibco.tibems.ufo.*;
 
-public class TrainListener{
-    
+public class TrainListener {
+
     private static String trainName;
+
     /*-----------------------------------------------------------------------
      * parseArgs
      *----------------------------------------------------------------------*/
-    private static void parseArgs(String[] args)
-    {
-        int i=0;
+    private static void parseArgs(String[] args) {
+        int i = 0;
 
-        while(i < args.length)
-        {
-            if (args[i].compareTo("-trainname")==0)
-            {
-                
-                trainName = args[i+1];
+        while (i < args.length) {
+            if (args[i].compareTo("-trainname") == 0) {
+
+                trainName = args[i + 1];
                 i += 2;
-                
-            }   
+
+            }
         }
     }
-    
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         parseArgs(args);
         Scanner sc = new Scanner(System.in);
         String lineSubscribed = "";
-        
-        TrainMsgConsumer weatherTopic = new TrainMsgConsumer("localhost","t.weather",TrainMsgConsumer.TOPIC);
+        System.out.println("Please set server URL (Enter 1 for localhost)");
+        System.out.flush();
+
+        String serverURL = sc.next(); //Get the new serverURL and update it
+
+        if (serverURL.equals("1")) {
+            serverURL = "localhost";
+        }
+
+        TrainMsgConsumer weatherTopic = new TrainMsgConsumer(serverURL, "t.weather", TrainMsgConsumer.TOPIC);
         TrainMsgConsumer lineMessages;
-        
+
         System.out.println("====================================");
-        System.out.println("       Train Name: " +trainName+"    ");
+        System.out.println("       Train Name: " + trainName + "    ");
         System.out.println("====================================");
-        
+
         int line = 99;
-        
-        while (line < 1 || line > 2){
+
+        while (line < 1 || line > 2) {
             System.out.println("Please log in to the correct Line: ");
             System.out.println("1. East-West (ew)");
             System.out.println("2. North-South (ns)");
             line = sc.nextInt();
         }
-        
-        System.out.println("Please set server URL (Enter 1 for localhost)");
-        
-        String serverURL = sc.nextLine();
-        
-        if(serverURL.equals("1")){
-            serverURL = "localhost";
-        }
-        
-        if(line == 1){
-            lineMessages = new TrainMsgConsumer(serverURL,"t.ew.listener",TrainMsgConsumer.TOPIC);
+
+        if (line == 1) {
+            lineMessages = new TrainMsgConsumer(serverURL, "t.ew.listener", TrainMsgConsumer.TOPIC);
             lineSubscribed = "t.ew.listener";
-        }else{
-            lineMessages = new TrainMsgConsumer(serverURL,"t.ns.listener",TrainMsgConsumer.TOPIC);
+        } else {
+            lineMessages = new TrainMsgConsumer(serverURL, "t.ns.listener", TrainMsgConsumer.TOPIC);
             lineSubscribed = "t.ns.listener";
         }
-        
+
         System.out.println();
         System.out.println();
-        
+
         System.out.println("Listening for message from t.weather");
-        System.out.println("Listening for message from "+lineSubscribed);
-        
+        System.out.println("Listening for message from " + lineSubscribed);
+
         System.out.println();
-        
-        
 
     }
-    
-    
-  
+
 }
