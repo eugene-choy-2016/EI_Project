d2a83bc693772f7f7d634b629f6c844bdb725265
eugene.choy.2016 <eugene.choy.2016@sis.smu.edu.sg>
2018-03-27 08:39:40 +0800

    +Rename EI Diagrams to EI Presentation, Removed compileEMSFromGit.bat
    ~ Renamed EI Diagram to EI Presentation
    - Removed compileEMSFromGit.bat (Template only)

--- compileEMSFromGit.bat
+++
@@ -1,8 +0,0 @@
-copy <FileNameToCopy> C:\tibco\ems\6.0\samples\java
-cd C:\tibco\ems\6.0\samples\java
-CALL C:\tibco\ems\6.0\samples\java\setup.bat
-javac <FileToCompile>
-
-java <FileToRun>
-
-pause
